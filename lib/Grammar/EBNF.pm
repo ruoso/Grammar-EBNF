02e0f587bfa5631cd53035ebab73dce07f83a2af
Daniel Ruoso <daniel@ruoso.com>
2015-02-16 22:09:58 -0500

    Start creating the slang, but not functional yet

--- lib/Grammar/EBNF.pm
+++ lib/Grammar/EBNF.pm
@@ -1,4 +1,10 @@
-module Grammar::EBNF {
-    use Grammar::EBNF::MetaSyntax;
-    
+use Grammar::EBNF::MetaSyntax;
+sub EXPORT(|) {
+    role Grammar::EBNF::Slang {
+        rule statement_control:sym<ebnf-grammar> {
+            <sym> <longname> \{ numerozero \= \"0\" \; \} 
+        }
+    }
+    nqp::bindkey(%*LANG, 'MAIN', %*LANG<MAIN>.HOW.mixin(%*LANG<MAIN>, Grammar::EBNF::Slang));
+    {}
 }
---
+++ t/004_slang.t
@@ -0,0 +1,6 @@
+use Test;
+use Grammar::EBNF;
+ebnf-grammar A::B {
+  numerozero = "0";
+};
+done();
