49212b053ef1751427a77a1d5bc96ffd21d89d2c
Daniel Ruoso <daniel@ruoso.com>
2015-02-16 22:18:49 -0500

    move forward a bit, but failing in a strange way

--- lib/Grammar/EBNF.pm
+++ lib/Grammar/EBNF.pm
@@ -2,9 +2,15 @@ use Grammar::EBNF::MetaSyntax;
 sub EXPORT(|) {
     role Grammar::EBNF::Slang {
         rule statement_control:sym<ebnf-grammar> {
-            <sym> <longname> \{ numerozero \= \"0\" \; \} 
+            <sym> <longname> \{ <Grammar::EBNF::MetaSyntax::TOP> \} 
+        }
+    }
+    role Grammar::EBNF::Actions {
+        method statement_control:sym<ebnf-grammar>(Mu $/ is rw) {
+            print "HERE!";
         }
     }
     nqp::bindkey(%*LANG, 'MAIN', %*LANG<MAIN>.HOW.mixin(%*LANG<MAIN>, Grammar::EBNF::Slang));
+    nqp::bindkey(%*LANG, 'MAIN-actions', %*LANG<MAIN-actions>.HOW.mixin(%*LANG<MAIN-actions>, Grammar::EBNF::Actions));
     {}
 }
